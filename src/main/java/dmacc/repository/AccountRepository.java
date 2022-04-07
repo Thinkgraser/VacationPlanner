5f4a9edfe0e94b93ec95b966966bbdbd839bf0c0
colbywemer <78008250+colbywemer@users.noreply.github.com>
2022-04-07 13:59:56 -0500

    Add files via upload

--- src/main/java/dmacc/repository/AccountRepository.java
+++ src/main/java/dmacc/repository/AccountRepository.java
@@ -1,10 +1,12 @@
 package dmacc.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import dmacc.beans.Account;
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long>{
-
+	public Account findOneByEmail(String email);
 }
