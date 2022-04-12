75fe67913a32ca40cd71bf30c22b023e29600427
callbee20 <callbee@dmacc.edu>
2022-04-11 22:43:12 -0500

    Added variables to hotel.java and added a hotel.html file in order to add hotels to database

--- src/Hotel.java
+++
@@ -1,9 +0,0 @@
-
-/**
- * @author Carson Allbee callbee
- *CIS175 - Fall 2021
- * Apr 11, 2022
- */
-public class Hotel {
-
-}
---
+++ src/main/java/dmacc/beans/Hotel.java
@@ -0,0 +1,78 @@
+package dmacc.beans;
+
+/**
+ * @author Carson Allbee callbee
+ *CIS175 - Fall 2021
+ * Apr 11, 2022
+ */
+public class Hotel {
+
+	private long id;
+	private String hotelName;
+	private int roomNumber;
+	private boolean occupied;
+	
+	public Hotel(String hotelName, int roomNumber, boolean occupied) {
+		super();
+		this.hotelName = hotelName;
+		this.roomNumber = roomNumber;
+		this.occupied = occupied;
+	}
+	
+	public Hotel(int roomNumber, boolean occupied) {
+		super();
+		this.roomNumber = roomNumber;
+		this.occupied = occupied;
+	}
+	
+	/**
+	 * @return the id
+	 */
+	public long getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(long id) {
+		this.id = id;
+	}
+	/**
+	 * @return the hotelName
+	 */
+	public String getHotelName() {
+		return hotelName;
+	}
+	/**
+	 * @param hotelName the hotelName to set
+	 */
+	public void setHotelName(String hotelName) {
+		this.hotelName = hotelName;
+	}
+	/**
+	 * @return the roomNumber
+	 */
+	public int getRoomNumber() {
+		return roomNumber;
+	}
+	/**
+	 * @param roomNumber the roomNumber to set
+	 */
+	public void setRoomNumber(int roomNumber) {
+		this.roomNumber = roomNumber;
+	}
+	/**
+	 * @return the occupied
+	 */
+	public boolean isOccupied() {
+		return occupied;
+	}
+	/**
+	 * @param occupied the occupied to set
+	 */
+	public void setOccupied(boolean occupied) {
+		this.occupied = occupied;
+	}
+	
+	
+}
